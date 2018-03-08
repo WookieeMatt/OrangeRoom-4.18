9eee41f942e442907677e4d5545b209d1c413cb6
WOOKIEE-DESKTOP\mriml <mrimler1983@gmail.com>
2018-03-08 15:49:04 -0600

    catching back up, door moves

---
+++ Source/OrangeRoom/OpenDoor.cpp
@@ -0,0 +1,40 @@
+// Copyright 2018 Matt Rimler Leve by UA Asset Store
+
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Find owning Actor
+	AActor* Owner = GetOwner();
+
+	//Create rotator
+	FRotator NewRotation = FRotator(0.0f, -90.0f, 0.0f);
+
+	//Set rotation
+	Owner->SetActorRotation(NewRotation);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
--- Source/OrangeRoom/PositionReport.cpp
+++ Source/OrangeRoom/PositionReport.cpp
@@ -1,16 +1,18 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
-
+#include "Gameframework/Actor.h"
+#include "OrangeRoom.h"
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+	
 }
 
 
@@ -19,7 +21,9 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%S is at %s"), *ObjectName, *ObjectPos);
 	
 }
 
