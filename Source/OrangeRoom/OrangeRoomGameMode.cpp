ba7176406369e2a76135944aef12f1ecd1187418
WOOKIEE-DESKTOP\mriml <mrimler1983@gmail.com>
2018-03-09 14:56:42 -0600

    grabber attached and reporting

---
+++ Source/OrangeRoom/OrangeRoomGameMode.cpp
@@ -0,0 +1,7 @@
+// Copyright 2018 Matt Rimler Leve by UA Asset Store
+
+#include "OrangeRoomGameMode.h"
+
+
+
+
---
+++ Source/OrangeRoom/OrangeRoomGameMode.h
@@ -0,0 +1,20 @@
+// Copyright 2018 Matt Rimler Leve by UA Asset Store
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OrangeRoomGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ORANGEROOM_API AOrangeRoomGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
