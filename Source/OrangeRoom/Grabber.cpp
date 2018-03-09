9ab79b2b25f67cc7f6e795675d0ad651bb448065
WOOKIEE-DESKTOP\mriml <mrimler1983@gmail.com>
2018-03-09 16:45:38 -0600

    linetrace

--- Source/OrangeRoom/Grabber.cpp
+++ Source/OrangeRoom/Grabber.cpp
@@ -1,7 +1,9 @@
 // Copyright 2018 Matt Rimler Leve by UA Asset Store
 
 #include "Grabber.h"
-
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -18,7 +20,6 @@ UGrabber::UGrabber()
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
-
 	UE_LOG(LogTemp, Warning, TEXT("Grabber is here!!"));
 	
 }
@@ -29,6 +30,34 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Get player viewpoint this tick
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation
+	);
+	//Log
+	/*UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"), 
+		*PlayerViewPointLocation.ToString(), 
+		*PlayerViewPointRotation.ToString()
+	)*/
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
+	//Draw a red trace in world
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		0.0f,
+		0.0f,
+		10.0f
+	);
+
+	//Ray cast out to reach distance
+
+	//see what we hit
 }
 
